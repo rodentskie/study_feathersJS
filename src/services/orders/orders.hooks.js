caf46584a6faf9c6278d34f3ef1c1fe03baaccb6
unknown <rodney.lingganay@biotechfarms.net>
2020-08-17 16:48:09 +0800

    ok

---
+++ src/services/orders/orders.hooks.js
@@ -0,0 +1,33 @@
+const { authenticate } = require("@feathersjs/authentication").hooks;
+
+module.exports = {
+  before: {
+    all: [],
+    find: [authenticate("jwt")],
+    get: [authenticate("jwt")],
+    create: [authenticate("jwt")],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+};
