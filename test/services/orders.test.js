a66be19e7700c0e1694d0afd8ca44bb7b5103327
unknown <rodney.lingganay@biotechfarms.net>
2020-08-18 10:34:01 +0800

    ok

--- test/services/orders.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'orders\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('orders');
-    expect(service).toBeTruthy();
-  });
-});
